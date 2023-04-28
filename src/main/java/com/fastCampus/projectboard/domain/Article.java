163d5fc02cd4cd4abbdc887fdd725554e872371a
YongKeon Kwon <cksm00@naver.com>
2023-04-28 23:26:28 +0900

    Article, ArticleComment 생성 및 변수 선언

---
+++ src/main/java/com/fastCampus/projectboard/domain/Article.java
@@ -0,0 +1,16 @@
+package com.fastCampus.projectboard.domain;
+
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private long id;
+    private String title;
+    private String hashtag;
+    private LocalDateTime createAt;
+    private String createBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+
+}
---
+++ src/main/java/com/fastCampus/projectboard/domain/ArticleComment.java
@@ -0,0 +1,15 @@
+package com.fastCampus.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private long id;
+    private Article article;
+    private String content;
+
+    private LocalDateTime createAt;
+    private String createBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+}
