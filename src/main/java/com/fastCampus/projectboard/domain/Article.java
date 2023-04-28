7266c93501f315f1dfbdbd06fc8e32944a5acb12
YongKeon Kwon <cksm00@naver.com>
2023-04-28 23:38:30 +0900

    Article, ArticleComment 생성 및 변수 선언

--- src/main/java/com/fastCampus/projectboard/domain/Article.java
+++ src/main/java/com/fastCampus/projectboard/domain/Article.java
@@ -5,12 +5,14 @@ import java.time.LocalDateTime;
 
 public class Article {
     private long id;
-    private String title;
-    private String hashtag;
-    private LocalDateTime createAt;
-    private String createBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private String title; //제목
+    private String content; //본문
+    private String hashtag; //해쉬태그
+    
+    private LocalDateTime createAt; //생성일시
+    private String createBy; //생성자
+    private LocalDateTime modifiedAt; //수정일시
+    private String modifiedBy; //수정자
 
 
 }
